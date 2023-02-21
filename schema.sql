095664b176db6bb1266ceec5ba41867c1c8f1dcd
Dell Pc <mudasir9757@gmail.com>
2023-02-21 21:50:02 +0500

    Change animals schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-id INT PRIMARY KEY NOT NULL,
-name VARCHAR(50) NOT NULL,
-date_of_birth DATE NOT NULL,
-escape_attempts INT NULL,
-neutered BOOLEAN NULL,
-weight_kg DECIMAL NOT NULL
+CREATE TABLE animals
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name character varying(50) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric NOT NULL,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
 )
