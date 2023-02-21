05e13f8fbd8e57adc6616fe51d95049a840d1f2c
Dell Pc <mudasir9757@gmail.com>
2023-02-21 20:58:41 +0500

    Create Table animals

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
 /* Database schema to keep the structure of entire database. */
+
+CREATE TABLE animals (
+id INT PRIMARY KEY NOT NULL,
+name VARCHAR(50) NOT NULL,
+date_of_birth DATE NOT NULL,
+escape_attempts INT NULL,
+neutered BOOLEAN NULL,
+weight_kg DECIMAL NOT NULL
+)
