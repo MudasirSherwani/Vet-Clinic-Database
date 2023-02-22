148e976e6bbbabdb0e7644e4c50a3a507c3e8850
Dell Pc <mudasir9757@gmail.com>
2023-02-22 17:10:18 +0500

    Required queries applied and take screenshots

--- queries.sql
+++ queries.sql
@@ -40,3 +40,31 @@ SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
 SELECT MAX(weight_kg) AS maximum_wgt, MIN(weight_kg)AS minimum_wgt FROM animals;
 SELECT AVG(escape_attempts) AS Avg_Escaped FROM animals 
 WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 1990 AND 2000;
+
+-- Project#: 3 Vet clinic database: query multiple tables
+
+SELECT an.name FROM animals an inner join owners ow on ow.id=an.owner_id 
+where ow.full_name='Melody Pond';
+
+SELECT an.name FROM animals an INNER JOIN species sp ON an.species_id = sp.id 
+WHERE sp.name='Pokemon';
+
+SELECT ow.full_name, an.name FROM owners ow LEFT JOIN animals an 
+ON ow.id = an.owner_id;
+
+SELECT COUNT(*) as No_Of_Animals, sp.name FROM animals an JOIN species sp
+ON an.species_id = sp.id GROUP BY sp.name;
+
+SELECT an.name as Animal_Name, sp.name as Species_Type, ow.full_name as Owener_Name 
+FROM animals an inner join owners ow 
+on an.owner_id = ow.id inner join species sp
+on an.species_id = sp.id 
+where ow.full_name = 'Jennifer Orwell' and sp.name = 'Digimon';
+
+SELECT ow.full_name, an.name, an.escape_attempts FROM animals an INNER JOIN owners ow
+on an.owner_id = ow.id
+where ow.full_name = 'Dean Winchester' and an.escape_attempts = 0;
+
+SELECT ow.full_name, COUNT(*) FROM animals an 	INNER JOIN owners ow 
+ON an.owner_id = ow.id GROUP BY ow.full_name ORDER BY COUNT(*) desc
+FETCH FIRST 1 ROWS ONLY;
