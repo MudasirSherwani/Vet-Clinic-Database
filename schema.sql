7a4b19fa828a2808b84bf78d777a5362fcd9bab3
Dell Pc <mudasir9757@gmail.com>
2023-02-22 13:14:21 +0500

    Alter table to add species column

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 /* Database schema to keep the structure of entire database. */
 
+-- Project#: 1 Vet clinic database: create animals table
 CREATE TABLE animals
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
@@ -10,3 +11,6 @@ CREATE TABLE animals
     weight_kg numeric NOT NULL,
     CONSTRAINT animals_pkey PRIMARY KEY (id)
 )
+
+-- Project#: 2 Vet clinic database: query and update animals table
+ALTER TABLE animals ADD species varchar(30) NULL;
