55fb25be4295534121ce391fae9fd92e91ce8c1f
Dell Pc <mudasir9757@gmail.com>
2023-02-23 13:09:52 +0500

    Create Table vets, specializations,  and visits with ref id

--- schema.sql
+++ schema.sql
@@ -42,3 +42,36 @@ add FOREIGN KEY (species_id) REFERENCES species(id);
 alter table animals
 add column owner_id integer,
 add FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
+-- Project#: 4 Vet clinic database: add "join table" for visits
+
+CREATE TABLE vets 
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name varchar(50) NOT NULL,
+    age integer NOT NULL,
+    date_of_graduation date NOT NULL,
+    CONSTRAINT vets_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE specializations (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    species_id integer NOT NULL,
+    vet_id integer NOT NULL,
+    CONSTRAINT specializations_pkey PRIMARY KEY (id)
+    );
+
+    alter table specializations
+    ADD FOREIGN KEY (species_id) REFERENCES species (id)
+    ADD FOREIGN KEY (vet_id) REFERENCES vets (id)
+
+    CREATE TABLE visits (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    animal_id integer NOT NULL,
+    vet_id integer NOT NULL,
+    date_of_visit date NOT NULL,
+    CONSTRAINT visits_pkey PRIMARY KEY (id),
+    FOREIGN KEY (animal_id) REFERENCES animals (id),
+    FOREIGN KEY (vet_id) REFERENCES vets (id)
+    )
