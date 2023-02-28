7e71d8ac943e57dfcd06d7209c0663fd137feb67
Dell Pc <mudasir9757@gmail.com>
2023-02-28 18:42:19 +0500

    Create table medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,13 @@
     date_of_birth date NOT NULL,
     CONSTRAINT patient_pkey PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS medical_histories 
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    admitted_at timestamp not null,
+    patient_id integer not null,
+    status varchar (30) null,
+    CONSTRAINT medical_histories_pkey PRIMARY KEY (id),
+    CONSTRAINT patient_medical_hist_fkey FOREIGN KEY (patient_id) REFERENCES patients (id)
+);
